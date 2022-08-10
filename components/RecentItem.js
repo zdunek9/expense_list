6dbbad2e925072461f586ca4af117b57d1846ef4
Michal <m.zdunczyk2@gmail.com>
2022-08-10 22:08:17 +0200

    updated

--- components/RecentItem.js
+++ components/RecentItem.js
@@ -17,16 +17,19 @@ function RecentItem({ id, name, price, date, navigate, cat }) {
     return newItem.icon;
   }
   return (
-    <Pressable onPress={editItemHandler}>
-      <View style={styles.itemWrapper}>
-        <Image style={styles.iconStyle} source={getCategories()} />
-        <View style={styles.nameDate}>
-          <Text style={[{ fontSize: 19 }]}>{name}</Text>
-          <TimeAgo timestamp={date} />
-        </View>
-        <View style={styles.price}>
-          <Text style={styles.pricee}>{price}$</Text>
-        </View>
+    <Pressable
+      onPress={editItemHandler}
+      style={({ pressed }) =>
+        pressed ? [styles.itemWrapper, styles.itemTap] : styles.itemWrapper
+      }
+    >
+      <Image style={styles.iconStyle} source={getCategories()} />
+      <View style={styles.nameDate}>
+        <Text style={[{ fontSize: 19 }]}>{name}</Text>
+        <TimeAgo timestamp={date} />
+      </View>
+      <View style={styles.price}>
+        <Text style={styles.pricee}>{price}$</Text>
       </View>
     </Pressable>
   );
@@ -43,6 +46,9 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     padding: 15,
     marginVertical: 10,
+  },
+  itemTap: {
+    backgroundColor: "#d9d9d9",
   },
   price: {
     justifyContent: "center",
